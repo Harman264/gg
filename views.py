bda72d3de6613a2a7de81e6ed1bf7c43c55eab4e
Harman264 <120935556+Harman264@users.noreply.github.com>
2022-12-20 12:46:05 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+import mysql.connector as sql
+un=''
+em=''
+ag=''
+pwd=''
+# Create your views here.
+def signaction(request):
+    global un,em,ag,pwd
+    if request.method=="POST":
+        m=sql.connect(host="localhost",user="root",passwd="harman",database='website')
+        cursor=m.cursor()
+        d=request.POST
+        for key,value in d.items():
+            if key=="user_name":
+                un=value
+            if key=="email":
+                em=value
+            if key=="age":
+                ag=value
+            if key=="password":
+                pwd=value
+        c="insert into users Values('{}','{}','{}','{}')".format(un,em,ag,pwd)
+        cursor.execute(c)
+        m.commit()
+    return render(request,'signup_pg.html')
+            
